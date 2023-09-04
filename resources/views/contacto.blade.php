bca1b37f35c1a4d716ae1d8458e017887e0dd51b
Aranza Vanessa <cybergoth2077@outlook.com>
2023-09-04 16:44:31 -0600

    Creacion de formulario

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>FORMULARIO DE CONTACTO</h1>
+    <form action="" method="POST">
+        <label for="nombre"></label>
+        <input name="nombre" id="nombre"><br>
+        <textarea name="comentario"></textarea><br>
+        <select name="tipo">
+            <option value="Alumno">Alumno</option>
+            <option value="Empleado">Empleado</option>
+        </select>
+        <input type="submit" value="Enviar">
+
+    </form>
+</body>
+</html>
